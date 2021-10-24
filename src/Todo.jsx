5a97c7a71b526c6f5dac85ba77118b5f17f6a1ad
JoseBorbon <joserborbon@gmail.com>
2021-10-24 07:47:17 -0400

    add Todo Component

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import './Todo.css';
+//flexbox css potential for todo to layer edit/remove buttons accordingly
 
 class Todo extends Component {
+  //pass down task, id, key of todo and method from parent component
+  //edit and delete buttons for todo
   render() {
-    return <div></div>;
+    return (
+      <div id={this.props.id} className="Todo">
+        <div>{this.props.task}</div>
+        <button>Edit</button>
+        <button>Remove</button>
+      </div>
+    );
   }
 }
 
