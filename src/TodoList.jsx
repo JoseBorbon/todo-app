8d1cdcde3f54318dd6a6ba36bfeb8e771f4cab4f
JoseBorbon <joserborbon@gmail.com>
2021-10-24 07:46:27 -0400

    add TodoList Component

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import TodoForm from './TodoForm';
 import Todo from './Todo';
+import './TodoList.css';
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todos: [] }; //holds Object[]
+    this.addTodo = this.addTodo.bind(this);
+  }
+
+  addTodo(newTodo) {
+    this.setState((st) => {
+      return { todos: [...st.todos, newTodo] };
+    });
+  }
   render() {
-    return <div>Sup</div>;
+    const todos = this.state.todos.map(({ task }) => <Todo task={task} />);
+    return (
+      <div id="Todo-list">
+        {this.state.todos.length ? todos : <div></div>}
+        <TodoForm handleAddTodo={this.addTodo} />
+      </div>
+    );
   }
 }
 
