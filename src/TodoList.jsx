08aba04f11874f360a9a5e136141f70fe2acd064
JoseBorbon <joserborbon@gmail.com>
2021-10-24 05:38:12 -0400

    base

---
+++ src/TodoList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+class TodoList extends Component {
+  render() {
+    return <div>Sup</div>;
+  }
+}
+
+export default TodoList;
