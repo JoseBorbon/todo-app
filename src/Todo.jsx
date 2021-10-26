9df130c2e40fb6bb60c3a9ffdd068f30df35276c
JoseBorbon <joserborbon@gmail.com>
2021-10-26 07:06:23 -0400

    add state to child component for form input

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -5,16 +5,44 @@ import './Todo.css';
 class Todo extends Component {
   //pass down task, id, key of todo and method from parent component
   //edit and delete buttons for todo
+  constructor(props) {
+    super(props);
+    this.state = { task: this.props.task, isEditing: false };
+    this.toggleForm = this.toggleForm.bind(this);
+    this.submitEdit = this.submitEdit.bind(this);
+  }
+
+  toggleForm() {
+    console.log('yo');
+    this.setState({ isEditing: !this.state.isEditing });
+  }
+
+  submitEdit() {
+    //need to pass task up to parent component for editing purposes
+  }
+
   render() {
-    return (
-      <div id={this.props.id} className="Todo">
-        <div>{this.props.task}</div>
-        <button>Edit</button>
-        <button onClick={() => this.props.handleRemove(this.props.id)}>
-          Remove
-        </button>
-      </div>
-    );
+    let result;
+    if (this.state.isEditing) {
+      result = (
+        <form onSubmit={this.submitEdit}>
+          <input type="text" name="task" defaultValue={this.state.task} />
+          <button onChange={this.change}>Save</button>
+        </form>
+      );
+    } else {
+      result = (
+        <div id={this.props.id} className="Todo">
+          <div>{this.state.task}</div>
+          <button onClick={this.toggleForm}>Edit</button>
+          <button onClick={() => this.props.handleRemove(this.props.id)}>
+            Remove
+          </button>
+        </div>
+      );
+    }
+
+    return result;
   }
 }
 
